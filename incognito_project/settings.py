201b2859dc10d900ebde9a0c57c4229110c96eda
Annanay-Vyas <annanayvyas48@gmail.com>
2026-03-22 01:50:53 +0530

    Fix syntax error and enable static root

--- incognito_project/settings.py
+++ incognito_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,3 +125,4 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels.layers.InMemoryChannelLayer'
     }
 }
+STATIC_ROOT = BASE_DIR / "staticfiles"
